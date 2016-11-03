4cccbecb5a2cee1fc045844e2dbca4458dc8fd5f
sevecol <sevecol@outlook.com>
2016-11-04 03:57:22 +0800

    Add files via upload

--- Media/shaders_screenspacereflection.hlsl
+++ Media/shaders_screenspacereflection.hlsl
@@ -19,23 +19,31 @@ struct VSInput
 };
 struct PSInput
 {
-	float4 position : SV_POSITION;
-	float2 uv : TEXCOORD;
+	float4 position 	: SV_POSITION;
+        float3 positionW	: positionW;		// World space position
+    	float3 normalW      	: normalW;
+	float2 uv 		: TEXCOORD;
 };
 cbuffer FrameBuffer : register(b0)
 {
 	float4x4 mView;
 	float4x4 mViewProj;
 	float4x4 mViewProjInv;
+	float4   vEyePos;
+	float4	 vCameraNF;
 };
 
 PSInput VSMain(VSInput input)
 {
 	PSInput result;
 
-	float3 position = input.position * 10.0f;
-	position.y -= 18.0f;
+	float3 position = input.position * 60.0f;
+	position.y -= 8.0f;
 	result.position = mul(float4(position, 1.0f), mViewProj);
+
+	result.positionW = position;
+	result.normalW = input.normal;
+
 	result.uv = input.uv;
 
 	return result;
@@ -44,10 +52,63 @@ PSInput VSMain(VSInput input)
 Texture2D g_texture0 : register(t0);
 Texture2D g_texture1 : register(t1);
 Texture2D g_texture2 : register(t2);
+TextureCube g_EnvironmentLight : register(t3);
 SamplerState g_sampler : register(s0);
+
+//RWStructuredBuffer<float>		g_Result  : register(u1);
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-	float4 color = g_texture1.Sample(g_sampler, input.uv);
-	return color;//float4(1,1,1,1);
+	float3 p = input.positionW;//float3(0.0,-8.0f,60.0f);//
+
+	float3 vToPoint = normalize(p - vEyePos.xyz);
+	float3 vReflection = reflect(vToPoint,float3(0,1,0));
+
+	uint nNum = 40;
+	float fStep = 40.0/nNum;
+
+	//
+	float4 color = float4(0,0,0,0);
+	float value = 1.0f;
+	for (uint i=1;i<nNum;++i)
+	{
+		float3 vPointW = p+vReflection*i*fStep;
+
+		float4 vPointP = mul(float4(vPointW,1.0f),mViewProj);
+		vPointP /= vPointP.w;
+
+		float2 uv = vPointP.xy;
+		uv += 1.0f;
+		uv /= 2.0f;	
+		uv.y = 1.0f - uv.y;
+
+		//g_Result[i] = 0.0f;
+		float4 tex = g_texture2.Sample(g_sampler,uv);
+		if (tex.a!=0.0f)
+		{
+			//g_Result[i] = 1.0f;
+			float depthR = mul(float4(vPointW,1.0f),mView).z;
+			if (depthR<=-60.0f)
+				continue;
+
+			float3 positionT = g_texture0.Sample(g_sampler,uv).xyz;
+			float depthT = mul(float4(positionT,1.0f),mView).z;
+
+			if (depthT>depthR)
+			{
+				//g_Result[0] = depthR;
+				//g_Result[1] = depthT;
+				//g_Result[2] = positionT.z;
+				color = tex;
+				value = (float)i/(float)nNum;
+				break;
+			}
+		}
+		
+	}
+
+	float4 environment = g_EnvironmentLight.SampleLevel(g_sampler,vReflection,0);
+	color = lerp(color,environment,value);
+
+	return float4(color.xyz,1.0f);
 }
