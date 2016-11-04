b597e828fe050d1898b978d71ab611efe22b761a
sevecol <sevecol@outlook.com>
2016-11-04 13:17:22 +0800

    Delete XSMAA.h

--- AntiAliased/XSMAA.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include "..\XDirectX12.h"
-
-bool InitSMAA(ID3D12Device* pDevice, UINT uWidth, UINT uHeight);
-void CleanSMAA();
-
-void SMAA_Render(ID3D12GraphicsCommandList* pCommandList);
