b3b8e588856a1153e4a44c0c2036fff28d927508
sevecol <sevecol@outlook.com>
2016-11-04 15:36:46 +0800

    Add files via upload

---
+++ PostProcess/XPostProcess.cpp
@@ -0,0 +1,45 @@
+
+#include "XPostProcess.h"
+
+#include "..\Resource\XTexture.h"
+#include "..\XHDR.h"
+
+extern UINT GetHandleHeapStart(XEngine::XDescriptorHeapType eType, UINT uCount);
+
+namespace PostProcess
+{
+	UINT										uRenderTargetBase, uGpuCSUBase;
+	XRenderTarget								*pRenderTarget = nullptr;
+}
+using namespace PostProcess;
+
+bool InitPostProcess(ID3D12Device* pDevice, UINT uWidth, UINT uHeight)
+{
+	uRenderTargetBase = GetHandleHeapStart(XEngine::XDESCRIPTORHEAPTYPE_RTV, 1);
+	uGpuCSUBase = GetHandleHeapStart(XEngine::XDESCRIPTORHEAPTYPE_GCSU, 1);
+
+	// RenderTarget
+	{
+		pRenderTarget = XRenderTarget::CreateRenderTarget(DXGI_FORMAT_R32G32B32A32_FLOAT, uWidth, uHeight, uRenderTargetBase, uGpuCSUBase, uGpuCSUBase);
+	}
+
+	return true;
+}
+void CleanPostProcess()
+{
+	SAFE_DELETE(pRenderTarget);
+}
+
+void PostProcess_Bind(ID3D12GraphicsCommandList* pCommandList)
+{
+	XRenderTarget* pHDRRenderTarget = HDR_GetRenderTarget();
+
+	// Copy 
+	pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(pHDRRenderTarget->GetResource(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_COPY_SOURCE));
+	pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(pRenderTarget->GetResource(), D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_RESOURCE_STATE_COPY_DEST));
+	pCommandList->CopyResource(pRenderTarget->GetResource(), pHDRRenderTarget->GetResource());
+	pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(pHDRRenderTarget->GetResource(), D3D12_RESOURCE_STATE_COPY_SOURCE, D3D12_RESOURCE_STATE_RENDER_TARGET));
+	pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(pRenderTarget->GetResource(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE));
+
+	pCommandList->SetGraphicsRootDescriptorTable(GRDT_SRV_POSTPROCESSTEXTURE, pRenderTarget->GetSRVGpuHandle());
+}
---
+++ PostProcess/XScreenSpaceReflection.cpp
@@ -0,0 +1,62 @@
+
+#include "XScreenSpaceReflection.h"
+
+#include "..\Resource\XGeometry.h"
+#include "..\Resource\XShader.h"
+#include "..\Resource\XTexture.h"
+#include "..\XHDR.h"
+
+namespace ScreenSpaceReflection
+{
+	XGraphicShader								*pShadingShader = nullptr;
+}
+using namespace ScreenSpaceReflection;
+
+bool InitScreenSpaceReflection(ID3D12Device* pDevice, UINT uWidth, UINT uHeight)
+{
+	// Shader
+	D3D12_INPUT_ELEMENT_DESC StandardVertexDescription[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "NORMAL",   0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 24, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "TANGENT",  0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	};
+	pShadingShader = XGraphicShaderManager::CreateGraphicShaderFromFile(L"Media\\shaders_screenspacereflection.hlsl", "VSMain", "vs_5_0", "PSMain", "ps_5_0", StandardVertexDescription, 4);
+
+	return true;
+}
+void CleanScreenSpaceReflection()
+{
+	XGraphicShaderManager::DelResource(&pShadingShader);
+}
+
+extern void RenderXZPlane(ID3D12GraphicsCommandList *pCommandList, XGraphicShader *pShader, XTextureSet *pTexture = nullptr);
+void ScreenSpaceReflection_Render(ID3D12GraphicsCommandList* pCommandList)
+{
+/*
+	IStructuredBuffer* pSBuffer = GetHDRSBuffer(0);
+	ID3D12Resource* pResultBuffer = GetHDRResultBuffer();
+	pCommandList->SetGraphicsRootDescriptorTable(4, pSBuffer->GetUAVGpuHandle());
+*/
+	RenderXZPlane(pCommandList, pShadingShader);
+/*
+	// GetResult
+	pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(GetSBuffer(1)->GetResource(), D3D12_RESOURCE_STATE_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_COPY_SOURCE));
+	pCommandList->CopyResource(pResultBuffer, GetSBuffer(1)->GetResource());
+	pCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(GetSBuffer(1)->GetResource(), D3D12_RESOURCE_STATE_COPY_SOURCE, D3D12_RESOURCE_STATE_UNORDERED_ACCESS));
+
+	float *pAddress = nullptr;
+	CD3DX12_RANGE readRange(0, 4 * sizeof(float));
+	pResultBuffer->Map(0, &readRange, reinterpret_cast<void**>(&pAddress));
+	//float fValue = *pAddress;
+
+	float fValue = 0.0f;
+	//for (UINT i = 0;i < g_uDispatchX * g_uDispatchY;++i)
+	{
+		fValue += pAddress[0];
+	}
+	pResultBuffer->Unmap(0, nullptr);
+	//g_pHDRConstantBuffers->fValue = fValue;
+*/
+}
